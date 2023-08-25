b903f6ee14d1f807bf81d921a11676005558501b
Anushree Oraon <anushreeoraon@gmail.com>
2023-08-25 19:49:53 +0530

    new update

--- main.py
+++ main.py
@@ -17,27 +17,37 @@ def choice():
 def list():
     dept=request.form.get('dept','')
     sem=request.form.get('sem','')
-@app.route('/searchengine')
-def searchengine():
-    return render_template('searchengine.html')
-@app.route('/search', methods=['POST'])
-def search():
-    search_term = request.form.get('search_term', '')
-    pdf_files = []
-    
-    if search_term:
-        documents_dir = os.path.join('static', 'Documents')
-        for filename in os.listdir(documents_dir):
+    pdf_files=[]
+    if sem=="1":
+        documents_dir = os.path.join('static', 'Documents','1st')
+    else:
+        documents_dir = os.path.join('static', 'Documents',dept,sem)
+    for filename in os.listdir(documents_dir):
             if filename.lower().endswith('.pdf'):
-                similarity_score = fuzz.partial_ratio(search_term.lower(), filename.lower())
-                if similarity_score >= 70:
-                    pdf_files.append(filename)
+                pdf_files.append(filename)
+    return render_template('results.html', pdf_files=pdf_files, dept=dept,sem=sem,dir=documents_dir )
+# @app.route('/searchengine')
+# def searchengine():
+#     return render_template('searchengine.html')
+# @app.route('/search', methods=['POST'])
+# def search():
+#     search_term = request.form.get('search_term', '')
+#     pdf_files = []
     
-    return render_template('results.html', pdf_files=pdf_files, search_term=search_term)
+    
+#     if search_term:
+#         documents_dir = os.path.join('static', 'Documents')
+#         for filename in os.listdir(documents_dir):
+#             if filename.lower().endswith('.pdf'):
+#                 similarity_score = fuzz.partial_ratio(search_term.lower(), filename.lower())
+#                 if similarity_score >= 70:
+#                     pdf_files.append(filename)
+    
+#     return render_template('results.html', pdf_files=pdf_files, search_term=search_term)
 
 @app.route('/download/<filename>')
-def download(filename):
-    documents_dir = os.path.join('static', 'Documents')
-    return send_from_directory(documents_dir, filename)
+def download(filename,dir):
+    #documents_dir = os.path.join('static', 'Documents')
+    return send_from_directory(dir, filename)
 if __name__ == '__main__':
     app.run(debug=True)
